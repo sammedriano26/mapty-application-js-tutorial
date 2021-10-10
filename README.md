5a5ff5e6c697fb602c12d790beff416ee3c11873
Sam Medriano <sam.medriano@outlook.com>
2021-10-11 01:35:19 +0800

    Add additional codes

---
+++ README.md
@@ -0,0 +1,49 @@
+# Udemy Mapty Project by Jonas Schmedtmann
+
+This project is my take on the Jonas Schmedtmann Mapty project.  He is focusing on the JavaScript for this project, meanwhile on my project - I will redesign the website using HTML and CSS, and play around with JavaScript.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### The challenge
+
+As a user, I want to:
+
+- log my running workouts with location, distance, time, pace and steps/minute.
+- Log my cycling workouts with location, distance, time, speed and elevation gain.
+- See all workouts at a glance.
+- See all workouts on a map.
+- See all workouts when I leave the app.
+
+### The Features needed
+- A map where users can click to add new workouts. Also the best way to get location coordinates.
+- Geolocation to display map at current location. Also, user-friendly.
+- Forms to input distance, time, pace, steps/minute, speed, and elevation gain.
+- Display all workouts in a list.
+- Display all workouts on the map.
+- Store workout data in the browser using local storage API.
+- On page reload, read the saved data from local storage and display to UI.
+
+### Links
+
+- Live Site URL: 
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Clean code using SASS
+- Vanilla JavaScript
+
+### What I learned
+
+- Project is still in progress.
--- js/script.js
+++ js/script.js
@@ -1,2 +1,90 @@
 "use strict";
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const form = document.querySelector(".form");
+const workoutContainer = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+let map, mapEvent;
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      map = L.map("map").setView(coords, 15);
+
+      L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
+        maxZoom: 20,
+        subdomains: ["mt0", "mt1", "mt2", "mt3"],
+      }).addTo(map);
+
+      // Event Listener on the map | Handling clicks on map
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert("Error: Could not retrieve your location");
+    }
+  );
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //   Clear Input Fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      "";
+
+  const { lat } = mapEvent.latlng;
+  const { lng } = mapEvent.latlng;
+
+  const coordinates = [lat, lng];
+
+  L.marker(coordinates)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+inputType.addEventListener("change", (e) => {
+  e.preventDefault();
+
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
